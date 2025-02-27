221312f592d3c758c118da150655c5f012ce1d5f
Sajad <sajadsoorni@gmail.com>
2025-02-27 23:12:36 +0000

    feat: build form with dynamic options and handle form submissions in React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: true },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 2, description: 'Charger', quantity: 1, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
 ];
+
 export default function App() {
   return (
     <div className="app">
@@ -19,10 +20,24 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +46,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -52,7 +67,7 @@ function Item({ item }) {
 function State() {
   return (
     <footer className="stats">
-      <em> 💼 You have X intems on your list, and you already packed X (X%)</em>
+      <em> 💼 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
 }
