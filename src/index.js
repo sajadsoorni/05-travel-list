2835cebe95120c4e7743e8479ee22e47cfefb044
Sajad <sajadsoorni@gmail.com>
2025-03-02 23:19:01 +0000

    Add Counter component with dynamic date calculation and reset functionality
    - Implement Counter component using useState to track count and step values
    - Add input range slider to adjust step (0 to 10)
    - Enable increment and decrement buttons to update count based on step value
    - Display dynamic messages for future and past dates
    - Include reset button to restore count and step to default values

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Exercise from './Exercise';
+import Challenge from './Challenge';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Exercise />
+    {/* <Exercise /> */}
+    <Challenge />
   </React.StrictMode>
 );
